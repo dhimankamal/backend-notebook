bafa6d559f2f68ad793058b236f20ba7b1d84b53
kamal <dhimank079@gmail.com>
2021-12-20 16:43:00 +0530

    push data & validator use

--- models/User.js
+++ models/User.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     name:{
         type: String,
         require: true
     },
-    email:{
-        type: String,
-        require: true,
-        unique:true
-    },
+    
     password:{
+        type: String,
+        require: true
+    },
+    mail:{
         type: String,
         require: true
     },
@@ -22,4 +23,6 @@ const UserSchema = new Schema({
 
 
 })
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,31 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const User = require('../models/User')
+const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
-router.get('/', (req,res)=>{
-   
-    res.json([])
-})
-
+//Create a user usin : POST "/api/auth"
+router.post(
+  '/',
+  [
+    body('name', 'Enter valid Name').isLength({ min: 3 }),
+    body('mail', 'Enter valid Mail').isEmail(),
+    body('password', 'Password Atleast 8 chars').isLength({ min: 8 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    User.create({
+      name: req.body.name,
+      mail: req.body.mail,
+      password: req.body.password
+    })
+      .then(user => res.json(user))
+      .catch(err => {
+        console.log(err)
+        res.json({ error: 'Please Eneter a unque for email',message: err.message })
+      })
+  }
+)
 module.exports = router
